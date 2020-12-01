816da5fd0799911b391d1393e6f19eb96c32b1ab
Ubuntu <ubuntu@ip-172-31-48-250.ec2.internal>
2020-12-01 17:29:09 +0000

    the first commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_01_155619) do
+ActiveRecord::Schema.define(version: 2020_12_01_115951) do
 
   create_table "booking_tours", force: :cascade do |t|
     t.integer "user_id"
@@ -43,7 +43,6 @@ ActiveRecord::Schema.define(version: 2020_12_01_155619) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
