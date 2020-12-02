085ee2fdd044e68dd887fc6c65da7936ef602beb
Ubuntu <ubuntu@ip-172-31-83-12.ec2.internal>
2020-12-02 17:16:48 +0000

    add function booking tour for users ; add price, image fields in tour table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+    email: 'admin@gmail.com',
+    password: '123456',
+    name: 'admin',
+    is_admin: true
+)
