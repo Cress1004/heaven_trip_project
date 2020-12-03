2d133c2e70c2b073031f3f1c6f9eb846bd3298ac
Ubuntu <ubuntu@ip-172-31-48-250.ec2.internal>
2020-12-03 04:33:34 +0000

    check input username

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
   
-  validates :name, presence: true
+  validates :name, presence: true, length: { minimum: 3, maximum: 25 }, format: { with: /\A[a-zA-Z0-9]+\Z/ }
   has_one_attached :avatar
 end
